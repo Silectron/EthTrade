e381abb802dfa17b49b4818668e240a3f7ac2c4a
Yi Heng Liu <yi.heng.liu@mail.mcgill.ca>
2021-11-15 21:35:43 -0500

    docs and fixes for coinbase portfolio apis

--- cbpro_run.py
+++ cbpro_run.py
@@ -13,18 +13,19 @@ def main():
     passphrase = config["CB_PASSPHRASE"]
     
     try:
-        client = cbpro.AuthenticatedClient(key, secret, passphrase)
-        # client = cbpro.AuthenticatedClient(key, secret, passphrase, api_url="https://api-public.sandbox.pro.coinbase.com") # sandbox
+        # client = cbpro.AuthenticatedClient(key, secret, passphrase)
+        client = cbpro.AuthenticatedClient(key, secret, passphrase, api_url="https://api-public.sandbox.pro.coinbase.com") # sandbox
         
     except Exception as e:
         print(e)
         return
     # Security is currency pair
-    portfolio = CoinbasePortfolio("ETH-USD", "USD", "ETH", client)
+    portfolio = CoinbasePortfolio(security_pair="BTC-USD", cbpro_client=client)
     
     print(portfolio.get_accounts())
     print(portfolio.get_quantity())
     print(portfolio.get_budget())
+    print(portfolio.place_limit_buy_order(40000, 100, None))
     print(portfolio.get_order_ids())
     
     return
--- ethtrade/portfolio/coinbase_portfolio.py
+++ ethtrade/portfolio/coinbase_portfolio.py
@@ -7,58 +7,169 @@ from ethtrade.portfolio import Portfolio
 
 
 class CoinbasePortfolio(Portfolio):
-    def __init__(self, security: str, account_currency: str, currency: str,
-                 cbpro_client: AuthenticatedClient):
-        super().__init__(security)
+    def __init__(self, security_pair: str, cbpro_client: AuthenticatedClient):
+        super().__init__(security_pair)
         self.client = cbpro_client
-        self.usd_account_id = self.get_account_by_currency(account_currency)
-        self.crypto_account_id = self.get_account_by_currency(currency)
+        self.currency_account_id = self._get_account_by_currency(security_pair.split('-')[1])
+        self.crypto_account_id = self._get_account_by_currency(security_pair.split('-')[0])
         self.max_retries = 5
+        
+    def _get_account_by_currency(self, currency: str) -> str:
+        """Get account id by currency.
 
-    def set_account_id(self, account_id: str):
-        self.usd_account_id = account_id
+        Args:
+            currency (str): Currency name (e.g. 'USD', 'USDT', 'ETH').
+
+        Raises:
+            Exception: Incorrect currency name.
+            Exception: Get accounts failed.
+            Exception: Account not found.
+            ConnectionError: raise exception
+
+        Returns:
+            str: Account id.
+        """        ''''''
+        try:
+            json_res = self.client.get_accounts()
+
+            if json_res is None:
+                raise Exception(
+                    "Get accounts failed:: likely incorrect account currency. Currency name for ETH is ETH")
+            elif not isinstance(json_res, list) and json_res["message"] is not None:
+                raise Exception("Get accounts failed::" + json_res["message"])
+
+            for account in json_res:
+                if 'currency' in account and account['currency'] == currency:
+                    return account['id']
+            raise ValueError("Account not found for " +
+                            currency + ". Recheck currency name")
+        except ValueError as e:
+            print(str(e))
+        except Exception as e:
+            raise ConnectionError(str(e))
+        
+    def _validate_price_quantity(price: float, quantity: float) -> None:
+        """Validate price and quantity helper function.
+
+        Args:
+            price (float): Price or limit price.
+            quantity (float): Quantity of crypto order.
+
+        Raises:
+            ValueError: Price or quantity is invalid.
+        """        ''''''
+        if price <= 0 or quantity <= 0:
+            raise ValueError("Price and Quantity must be greater than 0")
+        
+    def _convert_budget_to_size(self, limit_price: float, budget: float) -> float:
+        """Convert budget to size helper function.
+
+        Args:
+            limit_price (float): Limit price of crypto order.
+            budget (float): Budget in account currency.
+
+        Raises:
+            ValueError: Limit price or budget is invalid.
+
+        Returns:
+            float: Size of crypto order with precision of 6 decimal places.
+        """        ''''''
+        if limit_price <= 0 or budget <= 0:
+            raise ValueError("Limit price and budget must be greater than 0")
+            
+        return round(budget / limit_price, 6)
 
     def place_market_buy_order(self, budget: float,
                                fill_handler: Callable[
                                    [FilledOrder], None]) -> str:
+        """Place a market buy order.
+
+        Args:
+            budget (float): Budget in account currency.
+            fill_handler (Callable[ [FilledOrder], None]): [description]
+
+        Raises:
+            ValueError: Budget is invalid.
+            Exception: Market buy order failed.
+            ConnectionError: raise exception
+
+        Returns:
+            str: order id
+        """        ''''''
         try:
+            if budget <= 0:
+                raise ValueError("Budget must be greater than 0")
             json_res = self.client.place_market_order(
                 product_id=self.security, side="buy", funds=budget)
 
             if 'id' not in json_res:
                 raise Exception("Market buy order failed")
 
-        except ValueError as e:
-            print(str(e))
+            return json_res['id']
         except Exception as e:
             raise ConnectionError(str(e))
+            
 
-    '''Place limit buy order with price/unit for amount (size) of crypto'''
     def place_limit_buy_order(self, limit_price: float, budget: float,
                               fill_handler: Callable[
                                   [FilledOrder], None]) -> str:
+        """Place a limit buy order with price/unit for amount (size) of crypto.
+
+        Args:
+            limit_price (float): Limit price of crypto order.
+            budget (float): Budget in account currency to be used.
+            fill_handler (Callable[ [FilledOrder], None]): [description]
+
+        Raises:
+            ValueError: Limit price or budget is invalid.
+            Exception: Limit buy order failed.
+            ConnectionError: raise exception
+
+        Returns:
+            str: order id
+        """        ''''''
         try:
+            size = self._convert_budget_to_size(limit_price, budget)
+            
             json_res = self.client.place_limit_order(
                 product_id=self.security, side="buy",
-                price=limit_price, size=budget)
+                price=limit_price, size=size)
 
             if not isinstance(json_res, list) and 'id' not in json_res and json_res['message']:
                 raise Exception("Limit buy order failed::" +
                                 json_res["message"])
                 
             return json_res['id']
-        except ValueError as e:
-            print(str(e))
         except Exception as e:
             raise ConnectionError(str(e))
 
     def place_stop_buy_order(self, stop_price: float, limit_price: float,
                              budget: float, fill_handler: Callable[
                                  [FilledOrder], None]) -> str:
+        """Place a stop buy order with price/unit for amount (size) of crypto.
+
+        Args:
+            stop_price (float): Stop price of crypto order.
+            limit_price (float): Limit price of crypto order.
+            budget (float): Budget in account currency to be used.
+            fill_handler (Callable[ [FilledOrder], None]): [description]
+
+        Raises:
+            ValueError: Stop price or limit price or budget is invalid.
+            Exception: Stop buy order failed.
+            ConnectionError: raise exception
+
+        Returns:
+            str: order id
+        """        ''''''
         try:
+            if stop_price <= 0:
+                raise ValueError("Stop price must be greater than 0")
+            size = self._convert_budget_to_size(limit_price, budget)
+            
             json_res = self.client.place_stop_order(
                 product_id=self.security, side='buy',
-                price=stop_price, funds=budget)
+                price=stop_price, size=size)
 
             if not isinstance(json_res, list) and 'id' not in json_res:
                 raise Exception("Stop buy order failed::" +
@@ -74,7 +185,24 @@ class CoinbasePortfolio(Portfolio):
     def place_market_sell_order(self, quantity: float,
                                 fill_handler: Callable[
                                     [FilledOrder], None]) -> str:
+        """Place a market sell order.
+
+        Args:
+            quantity (float): Quantity of crypto order.
+            fill_handler (Callable[ [FilledOrder], None]): [description]
+
+        Raises:
+            ValueError: Quantity is invalid.
+            Exception: Market sell order failed.
+            ConnectionError: raise exception
+
+        Returns:
+            str: order id
+        """        ''''''
         try:
+            if quantity <= 0:
+                raise ValueError("Quantity must be greater than 0")
+            
             json_res = self.client.place_market_order(
                 product_id=self.security, side="sell", size=quantity)
 
@@ -92,7 +220,24 @@ class CoinbasePortfolio(Portfolio):
     def place_limit_sell_order(self, limit_price: float, quantity: float,
                                fill_handler: Callable[
                                    [FilledOrder], None]) -> str:
+        """Place a limit sell order with price/unit for amount (size) of crypto.
+
+        Args:
+            limit_price (float): Limit price of crypto order.
+            quantity (float): Quantity of crypto order.
+            fill_handler (Callable[ [FilledOrder], None]): [description]
+
+        Raises:
+            ValueError: Limit price or quantity is invalid.
+            Exception: Limit sell order failed.
+            ConnectionError: raise exception
+
+        Returns:
+            str: order id
+        """        ''''''
         try:
+            self._validate_price_quantity(limit_price, quantity)
+            
             json_res = self.client.place_limit_order(
                 product_id=self.security, side="sell",
                 price=limit_price, size=quantity)
@@ -111,7 +256,27 @@ class CoinbasePortfolio(Portfolio):
     def place_stop_sell_order(self, stop_price: float, limit_price: float,
                               quantity: float, fill_handler: Callable[
                                   [FilledOrder], None]) -> str:
+        """Place a stop sell order with price/unit for amount (size) of crypto.
+
+        Args:
+            stop_price (float): Stop price of crypto order.
+            limit_price (float): Limit price of crypto order.
+            quantity (float): Quantity of crypto order.
+            fill_handler (Callable[ [FilledOrder], None]): [description]
+
+        Raises:
+            ValueError: Stop price, limit price or quantity is invalid.
+            Exception: Place stop sell order failed.
+            ConnectionError: raise exception
+
+        Returns:
+            str: order id
+        """        ''''''
         try:
+            if stop_price <= 0:
+                raise ValueError("Stop price must be greater than 0")
+            self._validate_price_quantity(limit_price, quantity)
+            
             json_res = self.client.place_stop_order(
                 roduct_id=self.security, stop_type="loss",
                 price=limit_price, size=quantity, stop=stop_price)
@@ -128,8 +293,17 @@ class CoinbasePortfolio(Portfolio):
             raise ConnectionError(str(e))
 
     def get_budget(self) -> float:
+        """Get available budget in account currency.
+
+        Raises:
+            Exception: Get budget failed.
+            ConnectionError: raise exception
+
+        Returns:
+            float: available budget in account currency
+        """        ''''''
         try:
-            json_res = self.client.get_account(self.usd_account_id)
+            json_res = self.client.get_account(self.currency_account_id)
             '''
                 {
                     "id": "a1b2c3d4",
@@ -149,6 +323,15 @@ class CoinbasePortfolio(Portfolio):
             raise ConnectionError(str(e))
 
     def get_quantity(self) -> float:
+        """Get quantity of crypto holdings in account.
+
+        Raises:
+            Exception: Get crypto quantity failed.
+            ConnectionError: raise exception
+
+        Returns:
+            float: quantity of crypto holdings in account
+        """        ''''''
         try:
             json_res = self.client.get_account(self.crypto_account_id)
 
@@ -162,6 +345,16 @@ class CoinbasePortfolio(Portfolio):
             raise ConnectionError(str(e))
 
     def get_order_ids(self) -> List[str]:
+        """Get list of order ids for security pair.
+
+        Raises:
+            Exception: Get order ids failed.
+            Exception: Get order ids failed with message.
+            ConnectionError: raise exception
+
+        Returns:
+            List[str]: list of order ids
+        """        ''''''
         try:
             json_res = self.client.get_orders(product_id=self.security)
 
@@ -184,34 +377,65 @@ class CoinbasePortfolio(Portfolio):
             raise ConnectionError(str(e))
 
     def get_order_by_id(self, order_id: str) -> Union[Order, None]:
+        """Get order by order id.
+
+        Args:
+            order_id (str): order id
+
+        Raises:
+            Exception: Get order by id failed.
+            ConnectionError: raise exception
+
+        Returns:
+            Union[Order, None]: Order object
+        """        ''''''
         try:
             json_res = self.client.get_order(order_id)
 
             if 'id' not in json_res:
                 raise Exception("Get order failed::" + json_res["message"])
 
-            return Order(json_res)
+            return Order(json_res['id'])
         except ValueError as e:
             print(str(e))
         except Exception as e:
             raise ConnectionError(str(e))
 
     def cancel_order(self, order_id: str):
+        """Cancel order by order id.
+
+        Args:
+            order_id (str): order id to cancel
+
+        Raises:
+            Exception: Cancel order failed.
+            ConnectionError: raise exception
+        """        ''''''
         try:
             for _ in range(self.max_retries):
                 json_res = self.client.cancel_order(order_id)
                 if json_res is None or not json_res:
                     continue
                 else:
-                    return True
+                    return 
             raise Exception("Cancel order failed " +
                             str(self.max_retries) + " times")
+
         except ValueError as e:
             print(str(e))
         except Exception as e:
             raise ConnectionError(str(e))
 
     def get_accounts(self) -> List[str]:
+        """Get list of account ids for api account.
+
+        Raises:
+            Exception: Get accounts failed.
+            ConnectionError: raise exception
+
+        Returns:
+            List[str]: list of account ids
+        """        ''''''
         try:
             json_res = self.client.get_accounts()
 
@@ -223,23 +447,3 @@ class CoinbasePortfolio(Portfolio):
             print(str(e))
         except Exception as e:
             raise ConnectionError(str(e))
-
-    def get_account_by_currency(self, currency: str) -> str:
-        try:
-            json_res = self.client.get_accounts()
-
-            if json_res is None:
-                raise Exception(
-                    "Get accounts failed:: likely incorrect account currency. Currency name for ETH is ETH")
-            elif not isinstance(json_res, list) and json_res["message"] is not None:
-                raise Exception("Get accounts failed::" + json_res["message"])
-
-            for account in json_res:
-                if account['currency'] is not None and account['currency'] == currency:
-                    return account['id']
-            raise Exception("Account not found for " +
-                            currency + ". Recheck currency name")
-        except ValueError as e:
-            print(str(e))
-        except Exception as e:
-            raise ConnectionError(e)
